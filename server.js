02b5f6bece1a0e62e4b4c4552497e5520ca4d825
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-03-29 09:59:49 -0400

    Use helmet to prevent XSS as per specification

--- server.js
+++ server.js
@@ -3,12 +3,15 @@
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
+const helmet = require("helmet");
 
 const apiRoutes = require("./routes/api.js");
 const fccTestingRoutes = require("./routes/fcctesting.js");
 const runner = require("./test-runner");
 
 const app = express();
+
+app.use(helmet());
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
